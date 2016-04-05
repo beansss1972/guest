f03d3b52ab277f4641e4ec71df474747fb11eeaf
Chloe Zarte <cazarte@gmail.com>
2016-04-05 18:22:09 -0500

    fix submit button

--- guest.js
+++ guest.js
@@ -9,6 +9,24 @@ if (Meteor.isClient) {
   }
 });
 
+  Template.body.events({
+    'submit .new-comment': function(event) {
+      event.preventDefault();
+
+      Comments.insert({
+        text: event.target.comment.value,
+        CreatedAt: new Date(),
+        name: event.target.name.value
+    });
+
+      //TODO button stays 'clicked'
+      $('.new-comment').trigger("reset");
+      event.target.submit.blur();
+
+    }
+  });
+
+
   Template.comment.helpers({
     time: function() {
       return moment(this.createdAt).format('MMMM D, YYYY');
