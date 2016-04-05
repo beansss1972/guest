b4a54436dc69c10a95c4ebc20a98f59b8783ae4b
Chloe Zarte <cazarte@gmail.com>
2016-04-05 16:17:04 -0500

    add mongo capability

--- guest.js
+++ guest.js
@@ -1,19 +1,11 @@
+Comments = new Mongo.Collection('comments')
+
 if (Meteor.isClient) {
   Template.body.helpers({
   comments: function () {
-    return [{
-      name: 'Bob',
-      text: 'Bacon ipsum dolor amet leberkas tongue ham hock cow brisket capicola fatback corned beef tail ground round drumstick pig. Short ribs beef ribs shankle spare ribs jowl andouille pork doner sausage ball tip strip steak. Kielbasa jowl meatball bacon strip steak porchetta, beef chuck tongue andouille sausage filet mignon tail.',
-      createdAt: new Date()
-    }, {
-      name: 'Billy',
-      text: 'A much shorter message.',
-      createdAt: new Date()
-    }, {
-      name: 'Bernie',
-      text: 'Bacon ipsum dolor amet leberkas tongue ham hock cow brisket capicola fatback corned beef tail ground round drumstick pig. Short ribs beef ribs shankle spare ribs jowl andouille pork doner sausage ball tip strip steak. Kielbasa jowl meatball bacon strip steak porchetta, beef chuck tongue andouille sausage filet mignon tail.',
-      createdAt: new Date()
-    }];
+    return Comments.find({}, {
+      sort: { createdAt: -1 }
+    });
   }
 });
 
